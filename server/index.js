0a094efc891367f42d13cd9ab27ae9c461653986
Soumyadip Dey <soumyadipdey802@gmail.com>
2025-03-29 13:30:06 +0530

    on going

--- server/index.js
+++ server/index.js
@@ -6,14 +6,16 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+app.use(express.static(path.resolve("./public")));
+
 
 io.on("connection", socket => {
      socket.on("message", (message)=> {
           console.log("A new message came: ", message);
-     })
+          io.emit("message", message);
+     });
 });
 
-app.use(express.static(path.resolve("./public")));
 
 app.get("/", (req, res)=> {
      return res.sendFile("./public/index.html");
